7744e4096c4b7d46118fb70688d662f69e6756b9
Mimi Kim <kimmireu0921@gmail.com>
2023-06-21 05:46:25 +0900

    add MyFormWidget class extending to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,70 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'dart:math';
 
+class MyFormWidget extends StatefulWidget {
+  @override
+  _MyFormWidgetState createState() => _MyFormWidgetState();
+}
+
+class _MyFormWidgetState extends State<MyFormWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+  late TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+    _animation = Tween<double>(begin: 1.0, end: 0.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+    _textEditingController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Form Widget'),
+      ),
+      body: Column(
+        children: [
+          AnimatedBuilder(
+            animation: _animationController,
+            builder: (context, child) {
+              return Opacity(
+                opacity: _animation.value,
+                child: child,
+              );
+            },
+            child: TextFormField(
+              controller: _textEditingController,
+              decoration: InputDecoration(
+                labelText: 'Enter Text',
+              ),
+              onChanged: (value) {
+                if (_animationController.isDismissed) {
+                  _animationController.forward();
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
